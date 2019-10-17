7622715999fb676b3d4ea81ea7a0cee83faac465
SAMUEL FERREIRA DE CARVALHO <samuel.fcarvalho1@senacsp.edu.br>
2019-10-16 22:22:34 -0300

    Listagem de produtos

--- src/app/home/firebase-config.ts
+++ src/app/home/firebase-config.ts
@@ -3,7 +3,7 @@ export const firebaseconfig = {
     authDomain: 'appguitarshop.firebaseapp.com',
     databaseURL: 'https://appguitarshop.firebaseio.com',
     projectId: 'appguitarshop',
-    storageBucket: '',
+    storageBucket: 'appguitarshop.appspot.com',
     messagingSenderId: '170013665116',
     appId: '1:170013665116:web:8b8c20d0a2ea6ef3b00399'
 };
